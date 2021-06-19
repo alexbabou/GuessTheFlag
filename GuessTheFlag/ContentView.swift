3adfc1b57efee7ddd364b45aa2adc4272df51982
Alex Babou <ababou@dendev.net>
2021-06-19 05:28:30 -0600

    Added animations

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -17,6 +17,10 @@ struct ContentView: View {
     
     @State private var scoreCount = 0
     
+    @State private var rotateCorrect = 0.0
+    @State private var animateOpacity = 1.0
+    @State private var selectedFlag = 0
+    
     var body: some View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: [Color.white, Color.gray]), startPoint: .top, endPoint: .bottom)
@@ -38,12 +42,13 @@ struct ContentView: View {
                             .renderingMode(.original)
                             .clipShape(RoundedRectangle(cornerSize: CGSize(width: 20, height: 10)))
                             .shadow(color: .black, radius: 3)
-                            .opacity(0.8)
+                            .opacity(number != correctAnswer && showingScore ? animateOpacity : 1)
+                            .rotation3DEffect(.degrees(number == correctAnswer ? rotateCorrect : 0), axis: (x: 0, y: 1, z: 0))
                     }
                 }
                 
                 LinearGradient(gradient: Gradient(colors: [Color.yellow, Color.orange]), startPoint: .top, endPoint: .bottom).frame(width: 150, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    .padding(40)
+                    .padding(30)
                     .overlay(Text("Score: \(scoreCount)")
                                 .font(.title)
                                 .foregroundColor(.black)
@@ -62,12 +67,20 @@ struct ContentView: View {
             scoreCount += 5
             scoreTitle = "Correct Answer"
             scoreMessage = ""
+            
+            withAnimation {
+                rotateCorrect += 360
+                animateOpacity = 0.25
+            }
         } else {
             scoreCount -= 5
             scoreTitle = "Wrong Answer"
             scoreMessage = "That's the flag of \(countries[number])!\n"
+            
+            withAnimation {
+                animateOpacity = 0.25
+            }
         }
-        
         showingScore = true
     }
     
